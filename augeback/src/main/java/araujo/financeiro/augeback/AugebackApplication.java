f90a3c595833e4e50a4750a5e515fd1d41f3d567
Emerson <diegowebby@gmail.com>
2022-11-16 09:28:38 -0300

    first commit

---
+++ augeback/src/main/java/araujo/financeiro/augeback/AugebackApplication.java
@@ -0,0 +1,13 @@
+package araujo.financeiro.augeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AugebackApplication.class, args);
+	}
+
+}
---
+++ augeback/src/test/java/araujo/financeiro/augeback/AugebackApplicationTests.java
@@ -0,0 +1,13 @@
+package araujo.financeiro.augeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
